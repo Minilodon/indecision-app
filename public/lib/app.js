12cd44a8588e39627998a43c30f05d4ff52b1487
Pedro Soares <pedro.soareszl99@gmail.com>
2021-08-04 18:14:45 -0300

    babel configuration

---
+++ public/lib/app.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log("App.js is running");
+var TEMPLATE = /*#__PURE__*/React.createElement("p", null, "Eu sou um belo de um modelo");
+var APP_IP = document.getElementById("app");
+ReactDOM.render(TEMPLATE, APP_IP);
---
+++ src/app.js
@@ -0,0 +1,6 @@
+console.log("App.js is running")
+
+var TEMPLATE = <p>Eu sou um belo de um modelo</p>;
+var APP_IP = document.getElementById("app");
+
+ReactDOM.render(TEMPLATE,APP_IP);
