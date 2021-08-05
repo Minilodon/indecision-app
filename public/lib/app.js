0bac8838b344a1e67fd46c47a64aa60e4a5f3afd
Pedro Soares <pedro.soareszl99@gmail.com>
2021-08-04 21:40:29 -0300

    First lesson done!

--- public/lib/app.js
+++ public/lib/app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 console.log("App.js is running");
-var TEMPLATE = /*#__PURE__*/React.createElement("p", null, "Eu sou um belo de um modelo");
+var TEMPLATE = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Eu sou um belo de um modelo"), /*#__PURE__*/React.createElement("p", null, "Eu sou um texto qualquer"), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item um"), /*#__PURE__*/React.createElement("li", null, "Item dois")));
 var APP_IP = document.getElementById("app");
-ReactDOM.render(TEMPLATE, APP_IP);
+var MODELO = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Pedro Soares"), /*#__PURE__*/React.createElement("p", null, "Age: 22"), /*#__PURE__*/React.createElement("p", null, "Location: Salvador"));
+ReactDOM.render(MODELO, APP_IP);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 console.log("App.js is running")
 
-var TEMPLATE = <p>Eu sou um belo de um modelo</p>;
+var TEMPLATE = (
+    <div>
+        <h1>Eu sou um belo de um modelo</h1> 
+        <p>Eu sou um texto qualquer</p>
+        <ol>
+            <li>Item um</li>
+            <li>Item dois</li>
+        </ol>
+    </div>
+);
 var APP_IP = document.getElementById("app");
 
-ReactDOM.render(TEMPLATE,APP_IP);
+var MODELO = (
+    <div>
+        <h1>Pedro Soares</h1>
+        <p>Age: 22</p>
+        <p>Location: Salvador</p>
+    </div>
+);
+
+ReactDOM.render(MODELO,APP_IP);
